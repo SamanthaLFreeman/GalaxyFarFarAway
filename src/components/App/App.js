75dffe8d3b3ab431060c34b1ebd39b62fa4005a7
Brandy Mello <brandyamello@gmail.com>
2019-08-20 19:04:23 -0600

    Got movie data and add scrolling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,25 +9,57 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      data: [],
       films: []
     };
   }
 
   componentDidMount() {
+    // let array = [films]
+    // const promises = array.map(el => {
+    //   return fetch
+    // })
+    //   .then(response => response.json())
+
+    //   .then(data => this.setState({films: data.results}))
+    //   .then(() => console.log(this.state.films))
+    //   .catch(error => console.log(error));
+
+      // return Promise.all(filmInfo);
+
+    // const films = ['https://swapi.co/api/films']
+    // const promises = films.map(el => {
+    //   return fetch(el)
+    //   .then(response => response.json())
+    //   .then(data => ({...data}))
+    //   .catch(error => console.log(error))
+    // })
+    // return Promise.all(promises)
+    // .then(data => console.log(data[0].results))
+
     fetch('https://swapi.co/api/')
       .then(response => response.json())
       .then(data => this.fetchFilms(data.films))
+      
       .catch(error => console.log(error));
+
+
   }
+
+  // Promise.all([this.state.films]);
 
   fetchFilms = (filmData) => {
     fetch(`${filmData}`)
       .then(response => response.json())
-      .then(data => this.setState({films: data.results}))
+      .then(data => this.setState({films: data.results[0]}))
       .catch(error => console.log(error));
+      // return Promise.all(this.state.films);
   }
 
+
+
   render() {
+    console.log('data', this.state.films)
     return (
       <main>
         <h1>Galaxy Far Far Away</h1>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Movies.scss';
 
-const Movies = (props) => {
+const Movies = ({films}) => {
   return (
-    <section className='movies'>
-    Movies
-    {console.log(props.films)}
+    <section className='movie'>
+    <marquee width="40%" direction="up" scrollamount="6" behavior="scroll">
+      <p>{films.title}</p>
+      <p>{films.opening_crawl}</p>  
+      <p>{films.release_date}</p>
+    </marquee>   
     </section>
   );
 
