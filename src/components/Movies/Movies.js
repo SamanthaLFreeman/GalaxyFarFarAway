b75df3cc8fd649d77fd14c4335480b351ee2bd28
Alan Turing <freemasl.sf@gmail.com>
2019-08-20 20:57:42 -0600

    Clean up unnecessary comments and change films to film

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Movies.scss';
 
-const Movies = ({films}) => {
+const Movies = ({film}) => {
   return (
     <section className='movie'>
     <marquee width="40%" direction="up" scrollamount="6" behavior="scroll">
-      <p>{films.title}</p>
-      <p>{films.opening_crawl}</p>  
-      <p>{films.release_date}</p>
+      <p>{film.title}</p>
+      <p>{film.opening_crawl}</p>  
+      <p>{film.release_date}</p>
     </marquee>   
     </section>
   );
