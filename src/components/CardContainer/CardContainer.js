9636b61f7b345631626934ef9d8e8282ed254bd5
Alan Turing <freemasl.sf@gmail.com>
2019-08-22 15:14:20 -0600

    Clean up restructuring in CardContainer

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Card from '../Card/Card'
 import './CardContainer.scss';
 
-const CardContainer = (props) => {
-  const people = props.people.map(person => {
+const CardContainer = ({people}) => {
+  const peopleCards = people.map(person => {
     return <Card card={person} key={person.name}/>
   })
   return (
     <section className='card-container'>
-      {console.log(props)}
-      {people}
+      {console.log(people)}
+      {peopleCards}
     </section>
   )
 }
