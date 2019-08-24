3c67b1cac26ecc7d94013ba69e93804d5d6de421
Brandy Mello <brandyamello@gmail.com>
2019-08-24 13:28:44 -0600

    Add snapshots for people, planets, and vehicle

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -8,6 +8,44 @@ describe('Card', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should match the snapshot of the person card with all the data passed in correctly', () => {
+    const wrapper = shallow(<Card 
+      name={'Someone'}
+      language={'language'}
+      population={'population'}
+      homeworld={'homeworld'}
+      species={'species'}
+      toggleFavorite={'toggleFavorite'}
+      isFavorite={true}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot of the planet card with all the data passed in correctly', () => {
+    const wrapper = shallow(<Card
+      name={'name'}
+      terrain={'terrain'}
+      population={'population'}
+      climate={'climate'}
+      residents={'residents'}
+      toggleFavorite={'toggleFavorite'}
+      isFavorite={false}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should match the snapshot of the vehicle card with all the data passed in correctly', () => {
+    const wrapper = shallow(<Card
+      name={'name'}
+      model={'model'}
+      vehicleClass={'vehicleClass'}
+      numOfPassengers={'numOfPassengers'}
+      toggleFavorite={'toggleFavorite'}
+      isFavorite={false}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
   it('should invoke toggleFavorite method when the favorite button is clicked', () => {
     const toggleFavorite = jest.fn();
     const wrapper = shallow(<Card name={'Chewy'} type={'People'} toggleFavorite={toggleFavorite}/>)
