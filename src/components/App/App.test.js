9e71204692c0c68baf5ae4e6746af79f3a22247d
Brandy Mello <brandyamello@gmail.com>
2019-08-24 15:52:46 -0600

    Add working test for addToFavorites and another test for get random number, not working

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,6 +8,31 @@ describe('App', () => {
   expect(wrapper).toMatchSnapshot();
   });
 
+  it('should update the favorites array in state when addToFavorites is called', () => {
+    const mockCard = {
+      name:'Princess Sam',
+      language: 'Yiddish',
+      population: 3000000,
+      homeworld: 'Earth',
+      species: 'Human',
+      toggleFavorite: jest.fn(),
+      isFavorite: true
+    }
+    const wrapper = shallow(<App />)
+    wrapper.setState({people: mockCard});
+    expect(wrapper.state('favorites')).toEqual([]);
+    wrapper.instance().addToFavorites(mockCard);
+    expect(wrapper.state('favorites')).toEqual([mockCard]);
+    // wrapper.instance().addToFavorites(mockCard);
+    // expect(wrapper.state('favorites')).toEqual([]);
+  });
+
+  it('should return a number between 0 and 7', () => {
+    const wrapper = shallow(<App />)
+    wrapper.instance().getRandomNumber()
+    expect(wrapper.instance().getRandomNumber).toBeWithinRange(0, 7);
+  });
+
   
 });
   
