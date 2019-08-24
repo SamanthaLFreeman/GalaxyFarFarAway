e4ff77426a43d2b3c83dad57ece265573c31e3c8
Brandy Mello <brandyamello@gmail.com>
2019-08-24 17:16:30 -0600

    Add proptypes for movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Movies.scss';
 
 const Movies = ({film}) => {
@@ -15,3 +16,7 @@ const Movies = ({film}) => {
 }
 
 export default Movies;
+
+Movies.protoTypes = {
+  film: PropTypes.object
+}
