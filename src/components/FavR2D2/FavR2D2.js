660d3e1f9d650bcdad7d440af3787814692c9ef7
Brandy Mello <brandyamello@gmail.com>
2019-08-26 16:21:27 -0600

    Add styling for star image on view favorites button and general button styling and hover styles

--- src/components/FavR2D2/FavR2D2.js
+++ src/components/FavR2D2/FavR2D2.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import starImage from '../../Images/star3.png'
 import './FavR2D2.scss';
 
 const FavR2D2 = ({numOfFavs, checkAvail}) => {
   return (
     <section className='fav-R2D2'>
       <NavLink to='/favorites'>
-        <label>View Favorites</label>
-        <button className='favorites-btn' onClick={checkAvail}>{numOfFavs}</button>
+        <button className='favorites-btn' onClick={checkAvail}><img src={starImage} alt='' className='star-img'></img>{numOfFavs} View Favorites</button>
       </NavLink>
     </section>
 
